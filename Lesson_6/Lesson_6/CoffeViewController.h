2fc93e7a95b0846d385de7e91a0e3fac7f0cffcb
Denis <denismishchenko13@gmail.com>
2017-08-27 12:48:41 +0300

    Добавлен экран для домашнего задания

---
+++ Lesson_6/Lesson_6/CoffeViewController.h
@@ -0,0 +1,13 @@
+//
+//  CoffeViewController.h
+//  Lesson_6
+//
+//  Created by Denis on 27.08.17.
+//  Copyright © 2017 Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoffeViewController : UIViewController
+
+@end
---
+++ Lesson_6/Lesson_6/CoffeViewController.m
@@ -0,0 +1,33 @@
+//
+//  CoffeViewController.m
+//  Lesson_6
+//
+//  Created by Denis on 27.08.17.
+//  Copyright © 2017 Denis. All rights reserved.
+//
+
+#import "CoffeViewController.h"
+
+@interface CoffeViewController ()
+
+@end
+
+@implementation CoffeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+   
+}
+
+- (IBAction)backAction:(id)sender
+{
+     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+@end
